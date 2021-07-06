05f8abf5ea67542a374aac06cb24244cc628097a
Intiyaz10 <87025915+Intiyaz10@users.noreply.github.com>
2021-07-06 20:11:01 +0530

    Day 1 Assignment  Submitted

---
+++ Day1.py
@@ -0,0 +1,10 @@
+a=int(input("enter a value:"))
+b=int(input("enter b value:"))
+c=a+b
+print("Addition is : ",c)
+c=a-b
+print("subtraction is: ",c)
+c=a*b
+print("product is: ",c)
+c=a/b
+print("division is: ",c)
